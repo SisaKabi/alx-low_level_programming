1e88a644bc1af018e6dd47cc5d9bdc3aad7aa839
Busisa Kabi <sisakabi@icloud.com>
2023-02-16 17:43:58 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	char a[] = "Programming is like building a multilingual puzzle,";
+	
+	puts(a);
+
+	return (0);
+}
