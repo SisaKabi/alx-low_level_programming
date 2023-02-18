7d892f1a2e0a13974879e0f5824898ffce66dff3
Busisa Kabi <sisakabi@icloud.com>
2023-02-18 06:18:26 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - Entry point
@@ -7,8 +8,10 @@
  */
 int main(void)
 {
-	char a[] = "Programming is like building a multilingual puzzle,";
-	
+	char a[60];
+
+	strcpy(a,"Programming is like building a multilingual puzzle,");
+
 	puts(a);
 
 	return (0);
