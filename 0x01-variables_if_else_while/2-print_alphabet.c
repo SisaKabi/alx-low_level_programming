68a096a73f760919e3e0b087eab2dfffad441d03
Busisa Kabi <sisakabi@icloud.com>
2023-02-19 16:01:04 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z' ; n++) {
+		putchar(n);
+	}
+	return (0);
+}
