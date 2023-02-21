4a1b3cda5375949080de9e10a3db7c6248c6544f
Busisa Kabi <sisakabi@icloud.com>
2023-02-21 13:11:13 -0800

    Task 1

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long(int c);
+	long(long(int d);
+	float e;
+	printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\n", sizeof(a,b,c));
+	printf("Size of a long long int: %d byte(s)\nSize of a float: %d byte(s)\n", sizeof(d,e));
+	return (0);
+}
