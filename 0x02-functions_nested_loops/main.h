d337e5371089593001d013bbf564bbe1262c5e04
Busisa Kabi <sisakabi@icloud.com>
2023-02-22 13:30:51 -0800

    Task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-#include <unistd.h>
 #include <stdio.h>
 /**
  * _putchar - writes the character c to stdout
@@ -6,10 +5,6 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately
  */
-int _putchar (char c)
-{
-	return (write(1, &c, 1));
-}
 int printer (void)
 {
 	printf("_putchar""\n");
