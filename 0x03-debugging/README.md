f2a63188889fd41fbca9139b4eab4e6e1433a73c
Busisa Kabi <sisakabi@icloud.com>
2023-02-23 13:49:17 -0800

    Task 0

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+Created a markdown file for the current directory
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/* more headers go there */
+
+/**
+ * main - checks if the number is positive negative or zero
+ *
+ * Return: 0 for success, otherwise for failure
+ */
+int positive_or_negative(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	} else
+	{
+		if (n == 0)
+		{
+			printf("%i is zero\n", n);
+		} else
+		{
+			if (n < 0)
+			{
+				printf("%i is negative\n", n);
+			}
+		}
+	}
+	return (0);
+}
