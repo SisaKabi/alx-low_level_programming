636357d291da182e2415c8e76a930332b8e875aa
Busisa Kabi <sisakabi@icloud.com>
2023-02-22 16:34:36 -0800

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,10 @@
-#include "main.h"
 
 /**
  * alphabet_x10 - prints the alphabet 10 times to stdout
  *
  * Return: Always 0.
  */
-int main(void)
+int alphabet_x10(void)
 {
-	print_alphabet_x10();
 	return (0);
 }
