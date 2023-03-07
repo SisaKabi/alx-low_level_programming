d10c2dfeb9871199fe52dc72a3f311dccc99f9cc
Busisa Kabi <sisakabi@icloud.com>
2023-03-07 03:31:22 -0800

    Task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,10 +4,9 @@
  * mul - multiply two int
  * @a: first int
  * @b: second int
- * 
  * Return: addition of the multiplication.
  */
-int mul (int a, int b)
+int mul(int a, int b)
 {
 	return (a * b);
 }
