b00e156805cd4a789aa564e40860ee1f2499d0cd
Busisa Kabi <sisakabi@icloud.com>
2023-03-07 06:01:36 -0800

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,11 +9,11 @@ void print_most_numbers(void)
 {
 	int c;
 
-	for (c = 48; c <= 58; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (c != 50)
+		if (c != '2')
 		{
-			if (c != 52)
+			if (c != '4')
 			{
 				_putchar(c);
 			}
