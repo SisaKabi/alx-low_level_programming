a2c56faf9a2da08cc0dfe1dbc97364e39323e1e4
Busisa Kabi <sisakabi@icloud.com>
2023-03-08 11:39:52 -0800

    Task 0

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - calculates the factorial of the number
+ * @n: number for which factorial is calculated
+ *
+ * Return: -1 for error, 1 if n is one, 
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 1)
+		return (1);
+	else
+		return( n * factorial(n - 1));
+}
