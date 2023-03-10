d3e19fc4e7a7874b2dd4fc26d32121b0dc15668d
Busisa Kabi <sisakabi@icloud.com>
2023-03-10 05:45:22 -0800

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two given variables
+ * @a: first variable
+ * @b: second variable
+ *
+ * Return: Implicit
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
